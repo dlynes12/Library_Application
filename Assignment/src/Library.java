b8a0a422d76b05e94922fe7ff7b45c110dbe85b7
DJ <lynesdan>
2019-05-31 08:07:13 -0400

    Added book class with constructor, getters and setters. Also added part of library

--- Assignment/src/Library.java
+++ Assignment/src/Library.java
@@ -1,2 +1,15 @@
 public class Library {
+    public ArrayList<Book> book = new ArrayList<Book>;
+
+    public Library(ArrayList<Book> book) {
+        this.book = book;
+    }
+
+    public ArrayList<Book> getBook() {
+        return book;
+    }
+
+    public void setBook(ArrayList<Book> book) {
+        this.book = book;
+    }
 }
